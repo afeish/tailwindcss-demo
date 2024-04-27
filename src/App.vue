26f520cfcf6ae65cb904a3ad1ee0d249ec0978ca
neo.chen <neocxf@gmail.com>
2024-04-27 15:51:25 +0800

    feat: add formatter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { Bars4Icon, HomeIcon, QuestionMarkCircleIcon, EnvelopeIcon, ClockIcon } from "@heroicons/vue/20/solid";
-import { ref } from 'vue';
+import {Bars4Icon, HomeIcon, QuestionMarkCircleIcon, EnvelopeIcon, ClockIcon} from "@heroicons/vue/20/solid";
+import {ref} from 'vue';
+
 const recipes = ref([
   {
     id: 1,
@@ -36,7 +37,7 @@ const hidden = ref(true)
             <a href="/" class="hover:text-gray-800">Food Shop</a>
           </h1>
           <div :class="`px-4 cursor-pointer ${hidden ? 'md:hidden' : ''}`" id="burger" @click="hidden = !hidden">
-            <Bars4Icon class="h-6 w-6 text-gray-500" />
+            <Bars4Icon class="h-6 w-6 text-gray-500"/>
           </div>
         </div>
         <ul class="text-sm mt-6 hidden md:block" id="menu">
@@ -44,7 +45,7 @@ const hidden = ref(true)
             <a href="#" class="px-4 flex justify-end border-r-4 border-primary  items-center">
               <span class="border border-1 h-6 flex items-center justify-center">Home</span>
               <span
-                class="material-symbols-outlined ml-2 border border-1 h-6 flex items-center justify-center text-gray-500 text-xl">
+                  class="material-symbols-outlined ml-2 border border-1 h-6 flex items-center justify-center text-gray-500 text-xl">
                 home
               </span>
               <!-- <HomeIcon class="w-5 ml-2 text-gray-500" /> -->
@@ -53,13 +54,13 @@ const hidden = ref(true)
           <li class="text-gray-700 font-bold py-1 flex justify-start">
             <a href="#" class="px-4 flex justify-end border-r-4 border-white">
               <span class="ml-2">About</span>
-              <QuestionMarkCircleIcon class="w-5 ml-2 text-gray-500" />
+              <QuestionMarkCircleIcon class="w-5 ml-2 text-gray-500"/>
             </a>
           </li>
           <li class="text-gray-700 font-bold py-1 flex justify-start">
             <a href="#" class="px-4 flex justify-end border-r-4 border-white">
               <span>Contact</span>
-              <EnvelopeIcon class="w-5 ml-2 text-gray-500" />
+              <EnvelopeIcon class="w-5 ml-2 text-gray-500"/>
             </a>
           </li>
         </ul>
@@ -69,10 +70,10 @@ const hidden = ref(true)
     <main class="px-16 py-6 md:col-span-9 bg-gray-100">
       <div class="flex justify-center md:justify-end">
         <a href="#"
-          class="text-primary btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500">Log
+           class="text-primary btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500">Log
           in</a>
         <a href="#"
-          class="text-primary ml-2 btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500">Sign
+           class="text-primary ml-2 btn border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500">Sign
           up</a>
       </div>
 
@@ -88,8 +89,8 @@ const hidden = ref(true)
 
         <div class="mt-8 grid lg:grid-cols-3 gap-10">
           <!-- cards go here -->
-          <div class="card hover:shadow-lg" v-for=" recipe  in  recipes " :key="recipe.id">
-            <img :src="recipe.img" alt="stew" class="h-32 sm:h-48 w-full object-cover" />
+          <div class="card hover:shadow-lg" v-for="recipe  in  recipes " :key="recipe.id">
+            <img :src="recipe.img" alt="stew" class="h-32 sm:h-48 w-full object-cover"/>
             <div class="m-4">
               <span class="font-bold">{{ recipe.name }}</span>
               <span class="block text-gray-500 text-sm">{{ recipe.desc }}</span>
@@ -115,7 +116,7 @@ const hidden = ref(true)
 
       <div class="mt-12 flex justify-center">
         <div
-          class="bg-secondary-100 text-secondary-200 btn hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition ease-out duration-300">
+            class="bg-secondary-100 text-secondary-200 btn hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition ease-out duration-300">
           Load more
         </div>
       </div>
